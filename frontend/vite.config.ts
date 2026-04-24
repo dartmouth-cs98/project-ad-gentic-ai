7e800ef81ebe212c12a21c9a3f22e5ed43c812c3
Emmanuel Kasuti Makau <emmanuel.k.makau.jr.26@dartmouth.edu>
2026-04-24 13:24:29 -0400

    chore: changed frontend port back to 5173

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       host: true,
-      port: 3000,
+      port: 5173,
       headers: {
         'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
       },
